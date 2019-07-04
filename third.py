af0999cc6c45e3a187f09b31d4a1b1143a605280
AnushaSatram <40425206+AnushaSatram@users.noreply.github.com>
2019-07-04 10:37:29 +0530

    Create third.py

---
+++ third.py
@@ -0,0 +1,16 @@
+new=input()
+if(new.isalpha())
+  if(new=='a'):
+    print("Vowel")
+  elif(new=='e'):
+    print("Vowel")
+  elif(new=='i'):
+    print("Vowel")
+  elif(new=='o'):
+    print("Vowel")
+  elif(new=='u'):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")
