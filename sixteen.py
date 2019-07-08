5ff6d771ee35f3cb161e5a36f174faf62d63ed49
AnushaSatram <40425206+AnushaSatram@users.noreply.github.com>
2019-07-08 20:19:24 +0530

    Update sixteen.py

--- sixteen.py
+++ sixteen.py
@@ -1,7 +1,8 @@
 dg,kg=map(int,input().split())
 for p in range(dg+1,kg):
-    for x in range(2,p):
-      if((p%x)==0):
-        break
-    else:
-      print(p,end = ' ')
+    if(p>1):
+        for x in range(2,p):
+            if((p%x)==0):
+                break
+        else:
+            print(p,end = ' ')
