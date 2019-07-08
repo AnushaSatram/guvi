c603a01c0cb662ee1d0bfa8104ebba42ae35da72
AnushaSatram <40425206+AnushaSatram@users.noreply.github.com>
2019-07-08 19:09:18 +0530

    Update thirteen.py

--- thirteen.py
+++ thirteen.py
@@ -1,11 +1,10 @@
 digits=int(input())
 if(digits>1):
   for p in range(2,digits):
-    if(digits%p==0):
+    if((digits%p)==0):
       print("no")
       break
-    else:
-      print("yes")
-      break
+  else:
+    print("yes")
 else:
   print("no")
