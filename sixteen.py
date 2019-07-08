38f03d903db73dfe8859fdf650789cea53a98751
AnushaSatram <40425206+AnushaSatram@users.noreply.github.com>
2019-07-08 20:09:09 +0530

    Update sixteen.py

--- sixteen.py
+++ sixteen.py
@@ -2,8 +2,8 @@ dg=int(input())
 kg=int(input())
 for p in range(dg+1,kg):
   if(p>1):
-    for q in range(2,p):
-      if((p%q)==0):
+    for x in range(2,p):
+      if((p%x)==0):
         break
     else:
       print(p,end = ' ')
