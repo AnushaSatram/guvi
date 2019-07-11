8611770554249958bfe291748da0e41afefbd269
AnushaSatram <40425206+AnushaSatram@users.noreply.github.com>
2019-07-11 21:54:58 +0530

    Update thirty36.py

--- thirty36.py
+++ thirty36.py
@@ -1,7 +1,7 @@
 pq=str(input())
 cout=0
 for h in range(0,len(pq)):
-  if(pq[h].isalpha() or pq[h].isnumeric() or pq[h]==' ' or pq[h]=='.'):
+  if(pq[h].isalpha() or pq[h].isnumeric() or pq[h]==' '):
     continue
   else:
     cout=cout+1
